21502408046bf0987398f86b79136345ec6ca5f3
niushaohan <shaohan.niu@dfgroup.pro>
2020-10-13 10:43:05 +0800

    init project

---
+++ src/main/java/io/github/shniu/toolbox/App.java
@@ -0,0 +1,8 @@
+package io.github.shniu.toolbox;
+
+/**
+ * @author niushaohan
+ * @date 2020/10/13 10
+ */
+public class App {
+}
---
+++ src/test/java/io/github/shniu/toolbox/AppTest.java
@@ -0,0 +1,17 @@
+package io.github.shniu.toolbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author niushaohan
+ * @date 2020/10/13 10
+ */
+class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
